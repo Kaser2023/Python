41309b5615b273767b12572d70ac61bec9651058
kali <kaqmoh@gmail.com>
2024-10-21 23:22:50 +0300

    D

--- 8.Youtube_Downloder/yt-Downloader.py
+++ 8.Youtube_Downloder/yt-Downloader.py
@@ -1,6 +1,16 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
- 
+
+
+# ----------------------------------------------------------------
+# Coded by: Abdullah Alqurashi.
+# ----------------------------
+# Git-Hub: https://github.com/Kaser2023
+# Linked-In: https://www.linkedin.com/in/abdullah-alqurashi-a3777a224/
+# Date: 18.Rabi'a Alakhir. 1446 -  2024.Oct.21
+# ----------------------------------------------------------------
+
+
 url = "https://www.youtube.com/watch?v=TNAu6DvB9Ng"
  
 yt = YouTube(url, on_progress_callback = on_progress)
--- 8.Youtube_Downloder/yt-Playlist-Downloader.py
+++ 8.Youtube_Downloder/yt-Playlist-Downloader.py
@@ -2,7 +2,18 @@ from pytubefix import YouTube
 from pytubefix.cli import on_progress
  
 url = "https://www.youtube.com/watch?v=TNAu6DvB9Ng"
- 
+
+
+# ----------------------------------------------------------------
+# Coded by: Abdullah Alqurashi.
+# ----------------------------
+# Git-Hub: https://github.com/Kaser2023
+# Linked-In: https://www.linkedin.com/in/abdullah-alqurashi-a3777a224/
+# Date: 18.Rabi'a Alakhir. 1446 -  2024.Oct.21
+# ----------------------------------------------------------------
+
+
+
 yt = YouTube(url, on_progress_callback = on_progress)
 print(yt.title)
 
