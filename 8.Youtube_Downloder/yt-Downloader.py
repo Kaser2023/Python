f8d2e8c23d567cd169aa2d9189eb6a05eb2d25cc
kali <kaqmoh@gmail.com>
2024-10-21 23:25:53 +0300

    D

--- 8.Youtube_Downloder/yt-Downloader.py
+++ 8.Youtube_Downloder/yt-Downloader.py
@@ -4,7 +4,7 @@ from pytubefix.cli import on_progress
 
 # ----------------------------------------------------------------
 # Coded by: Abdullah Alqurashi.
-# ----------------------------
+# ------------------------------
 # Git-Hub: https://github.com/Kaser2023
 # Linked-In: https://www.linkedin.com/in/abdullah-alqurashi-a3777a224/
 # Date: 18.Rabi'a Alakhir. 1446 -  2024.Oct.21
