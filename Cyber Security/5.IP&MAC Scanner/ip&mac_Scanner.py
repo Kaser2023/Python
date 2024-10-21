0d51e0d9bf3b891283754446b91bead8b22d764c
kali <kaqmoh@gmail.com>
2024-10-22 01:22:32 +0300

    CyberSecurity

---
+++ Cyber Security/5.IP&MAC Scanner/ip&mac_Scanner.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+import subprocess
+
+# ----------------------------------------------------------------
+# Coded by: Abdullah Alqurashi.
+# ----------------------------
+# Git-Hub: https://github.com/Kaser2023
+# Linked-In: https://www.linkedin.com/in/abdullah-alqurashi-a3777a224/
+# Date: 13.Rabi'a Alakhir. 1446 -  2024.Oct.16
+# -----
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="00:11:00:22:00:33")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1,              verbose=False)[0]
+
+
+    kaser_clients = []
+    for kaser_element in answered_list:
+        kaser_client_dictionary = {"ip": kaser_element[1].psrc, "mac": kaser_element[1].hwsrc}
+        kaser_clients.append(kaser_client_dictionary)
+        #print(kaser_element[1].psrc + "\t" + kaser_element[1].hwsrc)
+
+    return kaser_clients
+
+
+def print_results(results):
+    print("---------------------------------------------")
+    print("IP\t\tAt MAC Address\t")
+    print("- - - - - - - - - - - - - - - - - - - - - - -")
+
+    for client in results:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+#scan_result = scan("192.168.1.1/24")
+scan_result = scan(input("[Hint!] Use the command [route -n(/24)] for getting the roter IP\n[-] Enter the Router IP for searching devices: "))
+
+print_results(scan_result)
